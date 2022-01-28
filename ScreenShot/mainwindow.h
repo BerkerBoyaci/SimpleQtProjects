3d244d8716069dfdf89577b3d18e9b44d28e0c75
BerkerBoyaci <41198169+BerkerBoyaci@users.noreply.github.com>
2022-01-28 09:06:21 +0200

    Added ScreenShot

---
+++ ScreenShot/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QScreen>
+#include <QMessageBox>
+#include <QPixmap>
+#include <QFileDialog>
+#include <QFileInfo>
+#include <QTimer>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_btnScreen_clicked();
+    void on_btnSave_clicked();
+    void shoot();
+
+private:
+    Ui::MainWindow *ui;
+    QPixmap m_image;
+    QTimer m_timer;
+};
+#endif // MAINWINDOW_H
